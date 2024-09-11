f1704b46c4a5a25f7150ca3b772d373356acc7a9
Jackie <78017364+jackiehozi@users.noreply.github.com>
2024-09-11 09:28:09 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,12 +9,31 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #commit3
+    return render_template('hello.html') #commit4
 
 # Nouvelle route pour la page de contact
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
+# Nouvelle route pour afficher les données météo de Tawarano
+@app.route('/tawarano/')
+def meteo():
+    # URL de l'API avec les données météo de Tawarano
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()  # Lire le contenu brut
+    json_content = json.loads(raw_content.decode('utf-8'))  # Décoder et charger en JSON
+    results = []
+
+    # Parcourir les éléments de la liste et extraire les dates et températures
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')  # Extraction du timestamp
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion en °C
+        results.append({'Jour': dt_value, 'temp': temp_day_value})  # Ajouter les résultats à la liste
+
+    # Retourner les résultats sous forme de JSON
+    return jsonify(results=results)
+
   
 if __name__ == "__main__":
   app.run(debug=True)
