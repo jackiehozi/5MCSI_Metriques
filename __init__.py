0e63a5cbdec4faa9cb526b39cc79bf23e685336f
Jackie <78017364+jackiehozi@users.noreply.github.com>
2024-09-11 09:45:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #commit6
+    return render_template('hello.html') #commit7
 
 # Nouvelle route pour la page de contact
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def contact():
+    return render_template("contact.html")
 
 # Nouvelle route pour afficher les données météo de Tawarano
 @app.route('/tawarano/')
