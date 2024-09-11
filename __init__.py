0bfbc9465f23fe0960b1a8fe59981724510a4909
Jackie <78017364+jackiehozi@users.noreply.github.com>
2024-09-11 10:41:49 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -46,30 +46,6 @@ def mongraphique():
 def histogramme():
     return render_template("histogramme.html")
 
-# Route pour afficher les commits
-@app.route('/commits/')
-def commits():
-    response = requests.get('https://api.github.com/repos/jackiehozi/5MCSI_Metriques/commits')
-    commits_data = response.json()
-    
-    # Extraire les minutes des commits
-    commit_minutes = {}
-    for commit in commits_data:
-        commit_time = commit['commit']['author']['date']
-        date_object = datetime.strptime(commit_time, '%Y-%m-%dT%H:%M:%SZ')
-        minute = date_object.minute
-        commit_minutes[minute] = commit_minutes.get(minute, 0) + 1
-    
-    # Préparer les données pour le graphique
-    results = [{'minute': minute, 'count': count} for minute, count in sorted(commit_minutes.items())]
-    return jsonify(results=results)
-
-# Route pour extraire les minutes d'une date
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    minutes = date_object.minute
-    return jsonify({'minutes': minutes})
 
 
   
