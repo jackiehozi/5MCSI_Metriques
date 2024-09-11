cfc5eccb21fb6f1cf92144f7a8f2cd56c48ca138
Jackie <78017364+jackiehozi@users.noreply.github.com>
2024-09-11 10:52:01 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 from datetime import datetime
+import requests
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
